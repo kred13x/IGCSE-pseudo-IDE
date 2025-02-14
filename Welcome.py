54edff24091ff307e244d4a1744b41b796c8fa16
kred13x <lydiamylu13@gmail.com>
2025-02-14 09:29:12 +0800

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,12 +1,15 @@
 import streamlit as st
 
-st.header("**Welcome to the IGCSE Pseudocode Interpreter!**")
+st.header("**Welcome to the IGCSE Pseudocode IDE!**")
 st.markdown("To start, click on any of these buttons:")
 st.page_link("pages/Command Line.py", icon = "💻")
 st.markdown("The **command line** allows you to enter **singular** lines of IGCSE pseudocode and get the result of each line. Perfect for shorter programs or just for testing small features!")
 st.markdown("")
 st.page_link("pages/Code Editor.py", icon = "📝")
 st.markdown("The **code editor** allows you enter **multiple** lines of IGCSE pseudocde and run all the lines at once. Suited for longer programs and for practising formal IGCSE questions!")
+st.markdown("")
+st.page_link("pages/Python Converter.py", icon = "🔁")
+st.markdown("The **Python converter** allows you to convert your pseudocode into Python! You can also download your pseudocode as a **Python file** here!")
 st.markdown("")
 st.page_link("pages/Documentation.py", icon = "📑")
 st.markdown("The **documentation** provides the IGCSE pseudocode syntax, as well as helpful tutorials!")
